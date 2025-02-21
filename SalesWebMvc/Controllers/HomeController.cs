77a1ee43dc04f5aea736b3f4c7e1350ecd25a557
mlsmarcio <mlsmarciogm@gmail.com>
2025-02-20 22:30:52 -0300

    First Model-Controller-View - Department

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -1,12 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
+using SalesWebMvc.Models.ViewModels;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using WebApplication1.Models;
 
-namespace WebApplication1.Controllers
+namespace SalesWebMvc.Controllers
 {
     public class HomeController : Controller
     {
